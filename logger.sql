b82a6569de0d2586f8d700a0a9da9ae01f48f85f
Dawid Elgan <dawid.elgan@g2a.com>
2022-05-15 20:06:44 +0200

    Cleaning code, adding OUTPUT parameter handling, updating readme.md

--- logger.sql
+++ logger.sql
@@ -1,7 +1,8 @@
 CREATE PROC dbo.logger
     @procedure_path NVARCHAR(100)
-,   @procedure_parameters NVARCHAR(100)
-,   @pass_log_id INT = 1
+,   @procedure_parameters NVARCHAR(500)
+,   @pass_log_id BIT = 0
+,   @catch_output BIT = 0
 
 /*
 =============================================================================
@@ -13,81 +14,111 @@ PARAMETERS:
         Example: @procedure_path = testing.dbo.load_data
     @procedure_parameters: a string with all parameters that procedure declared in @procedure_path needs.
         Example: @procedure_parameters = '@metric_name='asd',@value=100'
-    @pass_log_id: Possible states 1 and 0. If 1 procedure will pass log_id that was generated during
+    @pass_log_id: If 1 procedure will pass log_id that was generated during
         execution of this procedure so that the procedure declared in @procedure_path can receive it and further process it.
+        This can be helpful for instance when it's needed to write proc_outputs during the runtime:
+            update logger.dbo.logs
+                set proc_outputs = json_modify(proc_outputs, '$.runtime_output', json_query('{"something": 500}'))
+            where log_id = @log_id
+    @catch_output: If 1 then procedure from @procedure_path is required to have @json_log OUTPUT parameter in form of JSON. through which additional logs from inner procedure can be passed. It is required that whole procedure runs without any error to save the output.
 
 ADDITIONAL INFO:
-    A procedure declared in @procedure_path can return a single column of data that will be retrieved by this
-        procedure and it will store it in proc_params column. If there are multiple rows they will be aggregated to a single row.
     Procedure updates any logs where 'running' status remains too long and sets appropriate error message.
+
+KNOWN ISSUES:
+    If procedure from @procedure_path has TRY CATCH then error and status will not be logged properly. That inner procedure would have to THROW the error from CATCH block.
  =============================================================================
 */
 AS
 BEGIN TRY
     DECLARE @log_id NVARCHAR(50);
     DECLARE @procedure_call NVARCHAR(200);
-    DECLARE @catch_output_tbl TABLE(returned_string NVARCHAR(MAX));
-    DECLARE @catch_output_str NVARCHAR(MAX);
+    DECLARE @procedure_parameters_combined NVARCHAR(600)
+    DECLARE @output_log NVARCHAR(MAX);
 
     -- Changing status to 'failed' to any entry that has 'running status for a long time
         -- (Not all errors are caught by TRY/CATCH or user canceled execution).
-    UPDATE testing.dbo.proc_logs
+    UPDATE logger.dbo.logs
         SET status = 'failed'
         ,   error_message = 'Had running status for too long. Status automatically changed to failed.'
     WHERE status = 'running'
       AND DATEDIFF(hour, created_at, GETDATE()) >= 24;
 
     -- Creating initial log.
-    INSERT INTO testing.dbo.proc_logs(created_at, finished_at, proc_name, status, session_username, error_message, proc_params, proc_outputs)
-    SELECT  GETDATE(), NULL, @procedure_path, 'running', session_user, ERROR_MESSAGE(), @procedure_parameters, NULL
-    ;
-    SET @log_id = SCOPE_IDENTITY();
+    INSERT INTO logger.dbo.logs(created_at, finished_at, proc_name, status, session_username, error_message, proc_params, proc_outputs)
+    SELECT  GETDATE(), NULL, @procedure_path, 'running', session_user, ERROR_MESSAGE(), @procedure_parameters, '{}';
+    -- Storing log id. Inserting row to logs table should be generating new identity value.
+    SET @log_id = CAST(SCOPE_IDENTITY() AS VARCHAR(38));
 
-    -- Preparing dynamic SQL string.
+    -- Preparing parameters string for dynamic SQL string.
+    SET @procedure_parameters_combined = @procedure_parameters;
+
     IF @pass_log_id = 1
-        SET @procedure_call = CONCAT_WS(' ', @procedure_path, @procedure_parameters, ',@log_id =' + @log_id);
-    ELSE
-        SET @procedure_call = CONCAT_WS(' ', @procedure_path, @procedure_parameters)
+        SET @procedure_parameters_combined = CONCAT_WS(', ', @procedure_parameters_combined, '@log_id = ' + @log_id);
 
-    -- Executing dynamic SQL and storing potential value returned from stored procedure.
-    INSERT INTO @catch_output_tbl
-    EXEC(@procedure_call);
-    SET @catch_output_str = (SELECT STRING_AGG(returned_string, ', ') FROM @catch_output_tbl)
+    IF @catch_output = 1
+        SET @procedure_parameters_combined = CONCAT_WS(', ', @procedure_parameters_combined, '@json_log = @catching_output OUTPUT');
+
+    -- Preparing final dynamic SQL string.
+    SET @procedure_call = CONCAT_WS(' ', @procedure_path, @procedure_parameters_combined)
+
+    -- Executing dynamic SQL and storing output value returned from stored procedure.
+    IF @catch_output = 1
+        BEGIN
+            EXEC sp_executesql
+                @stmt = @procedure_call,
+                @params = N'@catching_output NVARCHAR(MAX) OUTPUT',
+                @catching_output = @output_log OUTPUT
+        END;
+    IF @catch_output = 0
+        BEGIN
+            EXEC(@procedure_call);
+        END;
 
     -- Updating initial log
-    UPDATE testing.dbo.proc_logs
+    UPDATE logger.dbo.logs
         SET status = 'complete'
         ,   finished_at = GETDATE()
-        ,   proc_outputs = @catch_output_str
-    WHERE log_id = @log_id
-    ;
+        ,   proc_outputs = JSON_MODIFY(proc_outputs, '$.final_procedure_output', JSON_QUERY(COALESCE(@output_log, '{}')))
+    WHERE log_id = @log_id;
 
 END TRY
 BEGIN CATCH
     -- Creating or updating initial log in case of an error.
-    MERGE INTO testing.dbo.proc_logs AS TGT
+    MERGE INTO logger.dbo.logs AS TGT
     USING (
         SELECT
             @log_id AS log_id
         ,   'failed' AS status
         ,   ERROR_MESSAGE() AS error_message
+        ,   ERROR_PROCEDURE() AS error_proc
+        ,   ERROR_LINE() AS error_line
+        ,   ERROR_SEVERITY() AS error_severity
+        ,   ERROR_STATE() AS error_state
         ,   GETDATE() AS created_at
         ,   GETDATE() AS finished_at
         ,   @procedure_path AS proc_name
         ,   session_user AS session_username
         ,   @procedure_parameters AS proc_params
-        ,   @catch_output_str AS proc_outputs
+        ,   @output_log AS proc_outputs
         ) AS SRC
         ON SRC.log_id = TGT.log_id
     WHEN NOT MATCHED BY TARGET
-        THEN INSERT(created_at, finished_at, proc_name, status, session_username, error_message, proc_params, proc_outputs)
-             VALUES(created_at, finished_at, proc_name, status, session_username, error_message, proc_params, proc_outputs)
+        THEN INSERT(created_at, finished_at, proc_name, status, session_username, proc_params
+                  , proc_outputs
+                  , error_message, error_proc, error_line, error_severity, error_state)
+             VALUES(SRC.created_at, SRC.finished_at, SRC.proc_name, SRC.status, SRC.session_username, SRC.proc_params
+                  , SRC.proc_outputs
+                  , SRC.error_message, SRC.error_proc, SRC.error_line, SRC.error_severity, SRC.error_state)
     WHEN MATCHED
         THEN UPDATE
             SET TGT.status = SRC.status
             ,   TGT.finished_at = SRC.finished_at
             ,   TGT.error_message = SRC.error_message
+            ,   TGT.error_proc = SRC.error_proc
+            ,   TGT.error_line = SRC.error_line
+            ,   TGT.error_severity = SRC.error_severity
+            ,   TGT.error_state = SRC.error_state
     ;
 END CATCH
 ;
-
--- readme.md
+++ readme.md
@@ -1,33 +1,56 @@
 # SQL Logger
 
-#### A procedure that generates and inserts logs into target table.
-
+#### A wrapper procedure that executes any provided procedure and generates logs into log table.
 
 ## Target table schema
 
 ```sql
-CREATE TABLE dbo.proc_logs(
+CREATE TABLE dbo.logs(
     log_id INT IDENTITY(1,1)
 ,   created_at DATETIME2
 ,   finished_at DATETIME2
 ,   proc_name NVARCHAR(100)
 ,   status NVARCHAR(50)
 ,   session_username NVARCHAR(50)
-,   error_message NVARCHAR(100)
-,   proc_params NVARCHAR(100)
-,   proc_outputs NVARCHAR(100)
+,   proc_params NVARCHAR(MAX)
+,   proc_outputs NVARCHAR(MAX)
+,   error_message NVARCHAR(4000)
+,   error_proc NVARCHAR(128)
+,   error_line INT
+,   error_severity INT
+,   error_state INT
 )
 ```
 
 ## Editing target table in procedure
 
 Target table of this stored procedure needs to be edited before use so that it references your table. 
-Currently it uses testing.dbo.proc_logs as target table.
+Currently it uses logger.dbo.logs as target table.
 
 
 ## Example
 
 ```sql
+-- inner procedure that will be wrapped by dbo.logger procedure. 
+CREATE PROC dbo.load_data
+    @metric_name NVARCHAR(100)
+,   @value INT
+,   @log_id INT
+,   @json_log NVARCHAR(MAX) OUTPUT
+AS
+BEGIN
+    DECLARE @finished_at DATETIME2;
+    SET @json_log = '{}';
+    
+    INSERT INTO dbo.data_table(log_id, created_at, metric_name, value)
+    SELECT @log_id, GETDATE(), @metric_name, @value;
+
+    SET @finished_at = GETDATE()
+    SET @json_log = JSON_MODIFY(@json_log, '$.finished_at', CONVERT(CHAR(19), @finished_at, 20))
+    SET @json_log = JSON_MODIFY(@json_log, '$.additional_data', JSON_QUERY('{"data": ["value1", "value2"]}'))
+END;
+
+-- target table of dbo.load_data procedure
 CREATE TABLE dbo.data_table(
     log_id INT
 ,   created_at DATETIME2
@@ -35,37 +58,30 @@ CREATE TABLE dbo.data_table(
 ,   value INT
 );
 
-CREATE PROC dbo.load_data
-    @metric_name NVARCHAR(100)
-,   @value INT
-,   @log_id INT
-AS
-BEGIN
-    INSERT INTO dbo.data_table(log_id, created_at, metric_name, value)
-    SELECT @log_id, GETDATE(), @metric_name, @value;
-
-    SELECT 'Some output' as d;
-END;
-
+-- executing dbo.load_data procedure by dbo.logger. 
+    -- By setting @pass_log_id = 1 we let dbo.logger pass @log_id to dbo.load_data
+    -- By setting @catch_output we let dbo.logger retrieve @json_log OUTPUT parameter from dbo.load_data
 EXEC dbo.logger
     @procedure_path = 'dbo.load_data'
-,   @procedure_parameters = '@metric_name=''test'',@value=100'
-,   @pass_log_id = 1;
+,   @procedure_parameters = '@metric_name=''test'', @value=100'
+,   @pass_log_id = 1
+,   @catch_output = 1;
 ```
 
 ## Results
 
 ```sql
-SELECT * FROM testing.dbo.proc_logs;
+SELECT * FROM logger.dbo.logs;
 ```
-|log_id	|created_at	           |finished_at	         |proc_name	    |status	   |session_username	|error_message	|proc_params	                |proc_outputs
-|---	|---	           |---	         |---	    |---	   |---	|---	|---	                |---
-|1	    |2022-04-14 23:15:05   |2022-04-14 23:15:05	 |dbo.load_data	|complete  |dbo		            |               |@metric_name='test',@value=100	|Some output
+
+log_id|created_at|finished_at|proc_name|status|session_username|proc_params|proc_outputs|error_message|error_proc|error_line|error_severity|error_state
+:-----:|:-----:|:-----:|:-----:|:-----:|:-----:|:-----:|:-----:|:-----:|:-----:|:-----:|:-----:|:-----:
+1|2022-05-15 18:57:07.7366667|2022-05-15 18:57:07.7400000|dbo.load_data|complete|dbo|@metric_name='test', @value=100|{"final_procedure_output":{"finished_at":"2022-05-15 18:57:07","additional_data":{"data": ["value1", "value2"]}}} 
 
 ```sql
-SELECT * FROM testing.dbo.data_table;
+SELECT * FROM logger.dbo.data_table;
 ```
 
 |log_id    |created_at          |metric_name      |value     |
 |----------|--------------------|-----------------|----------|
-|1         |2022-04-14 23:15:05 |test             |100       |
+|1         |2022-05-15 18:57:07.7400000 |test             |100       |
